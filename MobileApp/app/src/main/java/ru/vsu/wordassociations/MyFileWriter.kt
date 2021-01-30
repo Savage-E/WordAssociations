6f7b82a8f18bbd18642bf21b5fd0eb0ed4170d32
Savage-E <vlad.kotov1999@ya.ru>
2021-01-30 17:35:59 +0300

    fix size bugs

--- MobileApp/app/src/main/java/ru/vsu/wordassociations/MyFileWriter.kt
+++ MobileApp/app/src/main/java/ru/vsu/wordassociations/MyFileWriter.kt
@@ -3,8 +3,11 @@ package ru.vsu.wordassociations
 import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.opencsv.CSVWriter
 import java.io.File
 import java.io.FileOutputStream
+import java.io.FileWriter
+import java.io.IOException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
@@ -22,7 +25,7 @@ class MyFileWriter {
         val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH.mm.ss")
         val formattedDate = date.format(formatter)
         val fileName =
-            dataList[1] + " " + dataList[0][0] + " " + dataList[2][0] + " " + formattedDate + ".csv"
+            dataList[1] + " " + dataList[0][0] + " " + dataList[2][0] + " " + formattedDate + ".txt"
 
         //Создаем нужную директорию.
         val path = context.getExternalFilesDir(null)
@@ -30,24 +33,52 @@ class MyFileWriter {
         letDirectory.mkdirs()
         val file = File(letDirectory, fileName)
 
-        //Далее пишем в файл наши данные
-        file.appendText(dataList[1] + " ", Charsets.UTF_8)
-        file.appendText(dataList[0] + " ", Charsets.UTF_8)
-        file.appendText(dataList[2] + " ", Charsets.UTF_8)
-        file.appendText(dataList[3] + " ", Charsets.UTF_8)
-        file.appendText(dataList[4] + " ", Charsets.UTF_8)
-        file.appendText("\n", Charsets.UTF_8)
-        for (i in 0 until settings.size) {
-            file.appendText(settings[i] + ";", Charsets.UTF_8)
-        }
-        file.appendText("\n", Charsets.UTF_8)
+/*
+        val data: MutableList<Array<String>> = ArrayList()
+        data.add(arrayOf(dataList[1]+ " " +dataList[0]+ " " +dataList[2]+ " " +dataList[3]+ " " +dataList[4]))
+        val temp = null
+        temp?.let { settings.toCollection(it) }
 
-        for (i in associations) {
-            for (j in i) {
-                file.appendText("$j;", Charsets.UTF_8)
-            }
-            file.appendText("\n", Charsets.UTF_8)
+
+        temp?.let { data.addAll(1, it) }
+        for (array in associations)
+        {
+            data.add(array)
+
         }
+
+        var writer: CSVWriter? = null
+        try {
+            writer = CSVWriter(FileWriter(file))
+
+
+
+            writer.writeAll(data) // data is adding to csv
+            writer.close()
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }*/
+
+
+        //Далее пишем в файл наши данные
+         file.appendText(dataList[1] + " ", Charsets.UTF_8)
+         file.appendText(dataList[0] + " ", Charsets.UTF_8)
+         file.appendText(dataList[2] + " ", Charsets.UTF_8)
+         file.appendText(dataList[3] + " ", Charsets.UTF_8)
+         file.appendText(dataList[4] + " ", Charsets.UTF_8)
+         file.appendText("\n", Charsets.UTF_8)
+         for (i in 0 until settings.size) {
+             file.appendText(settings[i] + ";", Charsets.UTF_8)
+         }
+         file.appendText("\n", Charsets.UTF_8)
+
+         for (i in associations) {
+             for (j in i) {
+                 file.appendText("$j;", Charsets.UTF_8)
+             }
+             file.appendText("\n", Charsets.UTF_8 )
+         }
 
     }
 
